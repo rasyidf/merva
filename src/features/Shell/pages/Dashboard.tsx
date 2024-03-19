de6d06e9dc4974211aefc78cdf187f6adb815cd1
rasyidf <ufarasfa@gmail.com>
2024-03-19 08:52:14 +0700

    Update Dashboard

--- src/features/Shell/pages/Dashboard.tsx
+++ src/features/Shell/pages/Dashboard.tsx
@@ -1,16 +1,29 @@
 import { PageHeader } from "@/components/groups/Header";
-import { Paper, Stack } from "@mantine/core";
+import { StatsCard } from "@/components/groups/StatsCard/StatisticCard";
+import { Group, Paper, Stack } from "@mantine/core";
+import { DatePickerInput } from "@mantine/dates";
+import { Calendar, CalendarBlank } from "@phosphor-icons/react";
 
 export const Dashboard = () => {
   return (
     <Paper p={16}>
       <Stack gap={16}>
-        <PageHeader title="Dashboard" subtitle="Welcome to Merva Codebase" />
+        <Group justify="space-between">
+          <PageHeader title="Dashboard" subtitle="Welcome to Merva Codebase" />
+          <DatePickerInput
+            miw={200}
+            type="range"
+            leftSection={<CalendarBlank />}
+          />
+        </Group>
 
-
+        <Group mt={16} w="100%">
+          <StatsCard title={"Menunggu"} value={"2"} />
+          <StatsCard title={"Diterima"} value={"3"} />
+          <StatsCard title={"Ditolak"} value={"1"} />
+          <StatsCard title={"Total"} value={"6"} />
+        </Group>
       </Stack>
     </Paper>
   );
 };
-
-export default Dashboard;
