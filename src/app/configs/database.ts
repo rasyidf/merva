ce6a8fa443d8b742edb2c79c95720673adbf6f1a
rasyidf <28984914+rasyidf@users.noreply.github.com>
2024-09-19 01:14:24 +0700

    remove unnecesary packages

--- src/app/configs/database.ts
+++
@@ -1,5 +0,0 @@
-import { DATABASE_URL } from "@/shared/utils/constants";
-import Database from "better-sqlite3";
-import { drizzle } from "drizzle-orm/better-sqlite3"; 
-
-export const db = drizzle(new Database(DATABASE_URL));
